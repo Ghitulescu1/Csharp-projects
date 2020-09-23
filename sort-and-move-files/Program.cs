4cb8d4ee61ca0c6f32ba2bf4bfe9b7b6a5ff4121
Mihai Ghitulescu <lucian.ghitulescumihai@users.noreply.github.com>
2020-09-23 18:54:25 +0300

    Sort and move files

---
+++ sort-and-move-files/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.IO;
+using System.Collections;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string path = @"C:\Users\Gabi\Downloads";
+            string others = Path.Combine(path, "Others");
+            if (!Directory.Exists(others))
+            {
+                Directory.CreateDirectory(others);
+            }
+            Hashtable newFolders = new Hashtable
+            {
+                { "Documents", new string[] { ".pdf", ".doc", ".docx", ".rtf" } },
+                { "Pictures", new string[] { ".JPG" } },
+                { "Apps", new string[] { ".exe" } }
+            };
+            foreach (string dir in newFolders.Keys)
+            {
+                Console.WriteLine(dir);
+                string destPath = Path.Combine(path, dir);
+                if (!Directory.Exists(destPath))
+                {
+                    Directory.CreateDirectory(destPath);
+                    Console.WriteLine(destPath);
+                }
+                foreach (string value in (string[])newFolders[dir])
+                {
+                    string[] files = Directory.GetFiles(path);
+                    foreach (string file in files)
+                    {
+                        string extension = Path.GetExtension(file);
+                        string fileName = Path.GetFileName(file);
+                        if (extension == value)
+                        {
+                            Console.WriteLine(file);
+                            File.Move(file, Path.Combine(destPath, fileName));
+                        }
+                    }
+                }
+            }
+            string[] filesLeft = Directory.GetFiles(path);
+            Console.WriteLine("Others");
+            foreach (string file in filesLeft)
+            {
+                Console.WriteLine(file);
+                string fileName = Path.GetFileName(file);
+                File.Move(file, Path.Combine(others, fileName));
+                Console.WriteLine(Environment.MachineName);
+            }
+        }
+    }
+}
